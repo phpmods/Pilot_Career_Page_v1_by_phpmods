7e9f9382e8e1b1835791c05fda82363888ab4ea1
PHP-Mods <info@php-mods.eu>
2016-05-24 23:59:09 +0300

    Update to v1.2
    The module is now compatible with phpVMS 5.5. Made some other minor
    corrections too.

--- core/templates/career.php
+++ core/templates/career.php
@@ -1,9 +1,9 @@
 <?php
 ///////////////////////////////////////////////
-///  Pilot Career Page v1.1 by php-mods.eu  ///
+///  Pilot Career Page v1.2 by php-mods.eu  ///
 ///            Author php-mods.eu           ///
-///            Packed at 3/1/2013           ///
-///     Copyright (c) 2013, php-mods.eu     ///
+///           Packed at 25/05/2016          ///
+///     Copyright (c) 2016, php-mods.eu     ///
 ///////////////////////////////////////////////
 
 ?>
@@ -16,33 +16,25 @@
     <th>Pay Rate/Hour</th>
 	<th>Can Fly</th>
 	<th>Rank Image</th>
-	</tr>
+</tr>
 </thead>
 <tbody>
-<?php
-foreach($ranks as $rank)
-{
-?>
-<tr id="row<?php echo $rank->rankid;?>">
+<?php foreach($ranks as $rank) { ?>
+<tr>
 	<td align="center" width="30%"><?php echo $rank->rank; ?></td>
     <td align="center" width="15%"><?php echo $rank->minhours; ?></td>
     <td align="center" width="20%">$<?php echo $rank->payrate; ?>/hr</td>
 	<td align="center" width="20%"> 
     <?php $rankai = CareerData::getaircrafts($rank->rankid); 
-if(!$rankai)
-            {echo 'All the aircraft';}
-            else {
-foreach($rankai as $ran)
-{
-?><?php echo $ran->icao; ?> <?php } } ?></td>
-	<td align="center" width="25%"><img src="<?php echo $rank->rankimage; ?>" /></td>
-	</tr>
-<?php
-}
-?>
+		if(!$rankai) {echo 'All the aircraft';}
+		else {
+			foreach($rankai as $ran) {echo $ran->icao;} 
+		} ?></td>
+	<td align="center" width="25%"><img src="<?php echo $rank->rankimage; ?>" title="<?php echo $rank->rank; ?>" /></td>
+</tr>
+<?php } ?>
 </tbody>
 </table>
-<br />
 <h3>Awards</h3>
 <table id="tabledlist" class="tablesorter">
 <thead>
@@ -53,18 +45,12 @@ foreach($rankai as $ran)
 	</tr>
 </thead>
 <tbody>
-<?php
-foreach($generaward as $gen)
-{
-?>
-<tr id="row<?php echo $gen->awardid;?>">
+<?php foreach($generaward as $gen) { ?>
+<tr>
 	<td align="center" width="20%"><?php echo $gen->name; ?></td>
     <td align="center" width="60%"><?php echo $gen->descrip; ?></td>
-	<td align="center" width="20%"><img src="<?php echo $gen->image; ?>" /></td>
-	</tr>
-<?php
-}
-?>
+	<td align="center" width="20%"><img src="<?php echo $gen->image; ?>" title="<?php echo $gen->name; ?>" /></td>
+</tr>
+<?php } ?>
 </tbody>
 </table>
-<br />
